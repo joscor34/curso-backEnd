6254c7edf2da5aa833460b59d809519c60238a37
Jose Eduardo Esteva Cordova <zbrm34@gmail.com>
2020-01-16 17:05:25 -0600

    metodo POST agregado para obtener nombre

--- index.js
+++ index.js
@@ -2,8 +2,25 @@
 
 const express = require('express');
 const colors = require('colors');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-auth-token')
+
+  res.header('Access-Control-Expose-Headers', 'x-auth-token')
+
+  next()
+}); 
+
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
 
 app.get('/', (req, res) => {
   res.send('todo correcto en raiz').status(200);
@@ -12,6 +29,13 @@ app.get('/', (req, res) => {
 app.get('/nombre', (req, res) => {
   res.send('Hola mi nombre es jose').status(200);
 });
+
+app.post('/obtenerNombre', (req, res) => {
+  var name = req.body.name;
+  var apellido = req.body.lastName;
+
+  res.send(`Hola ${name} ${apellido}. Como estas?`).status(200)
+})
 
 app.listen(8000,() => {
   console.log(colors.rainbow('Felicidades tu aplicacion esta corriedo en http://localhost:8000'));
