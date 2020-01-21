2e0ff95422ca35c7a3e36d2c2d589eb89d51779f
Jose Eduardo Esteva Cordova <zbrm34@gmail.com>
2020-01-21 17:52:47 -0600

    Esquema del producto agregado

---
+++ models/product.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema ({
+    name: String,
+    price: Number,
+    description: String,
+    category: {
+        type: String,
+        enum: ['ropa', 'electronica', 'bebidas', 'alimentos']
+    }
+})
+
+module.exports = mongoose.model('Product', productSchema)
